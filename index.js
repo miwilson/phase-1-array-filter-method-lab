2118984828e2b81077a967875aca5b057755c191
Michaela Wilson <+>
2022-03-18 23:03:26 -0600

    commit tree 4b324d52a98a1974393cd7506008abb088c5e16f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+let value = ''
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ]
+
+function findMatching(drivers, string){
+    const name = drivers.filter(driversName => driversName.toLowerCase() === string.toLowerCase())
+    return name
+}
+
+function fuzzyMatch(drivers, string){
+    const name = drivers.filter(driversName => driversName.startsWith(string))
+    return name
+}
+
+function matchName(drivers, string){
+    const results = drivers.filter(drivers => drivers.name === string)
+    return results
+}
